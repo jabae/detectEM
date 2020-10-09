d8c53dd57308d1849b6e0b8dea1f4b64c0316300
Alexander Bae <jabae@vpn12-client-172-20-207-57.princeton.edu>
2020-10-09 15:36:35 -0400

    loss update

--- EMDetector/train/model.py
+++ EMDetector/train/model.py
@@ -39,7 +39,7 @@ class Model(nn.Module):
         ## Discrim
         for k in self.out_spec:
             
-            loss = F.binary_cross_entropy_with_logits(input=preds[k][valid], target=sample[k][valid])
+            loss = F.binary_cross_entropy_with_logits(input=preds[k], target=sample[k])
             losses[k] = loss.unsqueeze(0)
             
         return losses
